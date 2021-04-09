0c7fbac5c005229a89a68557ad16af7e66f438b5
theVebper <immortaldk@gmail.com>
2021-04-10 02:15:15 +0900

    Adding solved problems.

---
+++ src/baekjoon/levels/lv1/lv1_01_HelloWorld/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.levels.lv1.lv1_01_HelloWorld;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+
---
+++ src/baekjoon/levels/lv1/lv1_03_Cat/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.levels.lv1.lv1_03_Cat;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+/**
+ \    /\
+ )  ( ')
+ (  /  )
+ \(__)|
+ */
