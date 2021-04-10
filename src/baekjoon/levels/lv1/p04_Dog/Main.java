7291b3fd9011f9670a83bd93a1f7736db11adce3
theVebper <immortaldk@gmail.com>
2021-04-10 14:22:25 +0900

    Additional Problem solving

---
+++ src/baekjoon/levels/lv1/p04_Dog/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.levels.lv1.p04_Dog;
+
+/**
+ * |\_/|
+ * |q p|   /}
+ * ( 0 )"""\
+ * |"^"`    |
+ * ||_/=\\__|
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
---
+++ src/baekjoon/levels/lv1/p05_Addition/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.levels.lv1.p05_Addition;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //todo find the reason why it is a compile error. [v]
+        // - I didn't put import in the submission.
+
+        /*
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = a + b;
+
+        System.out.println(c);
+        */
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+        scanner.close();
+    }
+}
