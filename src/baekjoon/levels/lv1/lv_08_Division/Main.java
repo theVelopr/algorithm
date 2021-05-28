6eb37babd6267a2fd99c03475eba178b7e11e7f7
theVelopr <immortaldk@gmail.com>
2021-05-28 10:08:56 +0900

    added: algorithm lv1 p08.

---
+++ src/baekjoon/levels/lv1/lv_08_Division/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.levels.lv1.lv_08_Division;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        if (a > 0 && b < 10){
+            double c = a/b;
+            System.out.println(c);
+        }
+        sc.close();
+    }
+}
