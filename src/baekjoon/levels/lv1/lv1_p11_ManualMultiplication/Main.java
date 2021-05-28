7fb8839728f7882294ae9eb0f2e452dddf274d30
theVelopr <immortaldk@gmail.com>
2021-05-28 16:13:20 +0900

    temp: will be continued at home.

--- src/baekjoon/levels/lv1/lv1_p11_ManualMultiplication/Main.java
+++ src/baekjoon/levels/lv1/lv1_p11_ManualMultiplication/Main.java
@@ -1,4 +1,18 @@
 package baekjoon.levels.lv1.lv1_p11_ManualMultiplication;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // toString으로 받아서 값을 쪼개고 계산한 후 substring으로 표현하면 될 것 같은데...?
+        // 일단 업무부터. 
+
+
+
+    }
 }
